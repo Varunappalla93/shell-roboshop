81705e12456e86d08ac2b7973ea49225fd59a90c
Varunappalla1993 <an.varun1993@gmail.com>
2026-06-18 12:48:01 +0530

    shroboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,7 +15,7 @@ do
     --output text )
 
     if [ $instance == "frontend" ]; then
-       $IP=$(aws ec2 describe-instances \
+       IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
